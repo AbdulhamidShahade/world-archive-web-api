41cb9bd9cfc1c8d1f75cf1a08b29f23e4a9f16a5
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-08-29 15:50:54 +0300

    Add region domain model

---
+++ WorldArchive.Domain/Models/Region.cs
@@ -0,0 +1,12 @@
+﻿using WorldArchive.Domain.Models.Base;
+
+namespace WorldArchive.Domain.Models
+{
+    public class Region : EntityBase
+    {
+        public string Name { get; set; }
+
+
+        public List<Country> CountriesByRegion { get; set; }
+    }
+}
