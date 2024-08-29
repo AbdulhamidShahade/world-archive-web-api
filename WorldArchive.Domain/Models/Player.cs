4d8cd7f809472640ab2db9c628d1d281483a565f
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-08-29 16:17:09 +0300

    Add player domain model

---
+++ WorldArchive.Domain/Models/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WorldArchive.Domain.Models
+{
+    public class Player : Person
+    {
+
+        public Sport Sport { get; set; }
+
+
+        [NotMapped]
+        public override int Age
+        {
+            get
+            {
+                var today = DateTime.Today;
+                var age = today.Year - BirthDate.Year;
+
+                if (BirthDate.Date > today.AddYears(-age)) age--;
+
+                return age;
+            }
+        }
+
+        public override string GetFullName(bool reversed = false)
+        {
+            return (reversed) ? LastName + " " + FirstName : FirstName + " " + LastName;
+        }
+    }
+}
