5cbc80c39ff36e72f942759613ff2d9e3703c15c
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-08-29 15:51:17 +0300

    Add MovieActor model

---
+++ WorldArchive.Domain/Models/MovieActor.cs
@@ -0,0 +1,15 @@
+﻿using WorldArchive.Domain.Models.Base;
+
+namespace WorldArchive.Domain.Models
+{
+    internal class MovieActor : EntityBase
+    {
+        public int MovieId { get; set; }
+        public Movie Movie { get; set; }
+
+
+
+        public int ActorId { get; set; }
+        public Actor Actor { get; set; }
+    }
+}
