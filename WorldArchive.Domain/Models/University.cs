c1014568b393d0ddb65e001fc56641109ac14e1c
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-08-29 15:51:04 +0300

    Add university domain model

---
+++ WorldArchive.Domain/Models/University.cs
@@ -0,0 +1,12 @@
+﻿using WorldArchive.Domain.Models.Base;
+
+namespace WorldArchive.Domain.Models
+{
+    public class University : EntityBase
+    {
+        public string Name { get; set; }
+        public int NumberOfStudents { get; set; }
+        public int Image { get; set; }
+        public City City { get; set; }
+    }
+}
