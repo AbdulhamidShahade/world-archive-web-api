5aebe8d69a40bd0166a89ec181b34e98f2f66d49
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-08-29 16:17:19 +0300

    Add sport domain model

---
+++ WorldArchive.Domain/Models/Sport.cs
@@ -0,0 +1,13 @@
+﻿using WorldArchive.Domain.Models.Base;
+
+namespace WorldArchive.Domain.Models
+{
+    public class Sport : EntityBase
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+
+        public List<Player> PlayersBySport { get; set; }
+    }
+}
