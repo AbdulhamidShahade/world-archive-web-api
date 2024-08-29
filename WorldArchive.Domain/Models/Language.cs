9a27505337873b466e4f45ce8ffa985869fe9e88
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-08-29 15:50:24 +0300

    Add language domain model

---
+++ WorldArchive.Domain/Models/Language.cs
@@ -0,0 +1,13 @@
+﻿using WorldArchive.Domain.Models.Base;
+
+namespace WorldArchive.Domain.Models
+{
+    public class Language : EntityBase
+    {
+        public string Name { get; set; }
+        public int SpokenBy { get; set; }
+
+        public List<CountryLanguage> CountriesSpokenByLanguage { get; set; }
+        public List<MovieLanguage> MoviesByLanguage { get; set; }
+    }
+}
