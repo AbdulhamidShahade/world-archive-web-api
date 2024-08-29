3a152e716dd9c69bc931c829d6ac7cc90b289565
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-08-29 15:50:03 +0300

    Add country domain model

---
+++ WorldArchive.Domain/Models/Country.cs
@@ -0,0 +1,23 @@
+﻿using WorldArchive.Domain.Models.Base;
+
+namespace WorldArchive.Domain.Models
+{
+    public class Country : EntityBase
+    {
+        public string Name { get; set; }
+        public string CallingCode { get; set; }
+        public string capital { get; set; }
+        public Region Region { get; set; }
+        public int Area { get; set; }
+        public string Flag { get; set; }
+        public string CoatOfArms { get; set; }
+        public int Population { get; set; }
+        public Map Maps { get; set; }
+        public string StartOfWeek { get; set; }
+        public List<string> TimeZones { get; set; }
+
+
+        public List<CountryLanguage> LanguagesSpokenByCountry { get; set; }
+        public List<City> CitiesByCountry { get; set; }
+    }
+}
