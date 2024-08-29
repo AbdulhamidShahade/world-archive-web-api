e2e9a2c312be2623c5aa940bcce5641fbd235bd8
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-08-29 15:50:13 +0300

    Add food domain model

---
+++ WorldArchive.Domain/Models/Food.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorldArchive.Domain.Models.Base;
+
+namespace WorldArchive.Domain.Models
+{
+    public class Food : EntityBase
+    {
+        public string Name { get; set; }
+        public City City { get; set; }
+        decimal Price { get; set; }
+    }
+}
