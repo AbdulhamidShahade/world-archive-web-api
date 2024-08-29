befaf24a0c121c2706e542c505f85688fee1c127
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-08-29 15:52:05 +0300

    Add person domain model

---
+++ WorldArchive.Domain/Models/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using WorldArchive.Domain.Models.Base;
+
+namespace WorldArchive.Domain.Models
+{
+    public abstract class Person : EntityBase
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public int? Height { get; set; }
+        public abstract int Age { get; }
+
+        //reversed: LastName + FirstName
+        public abstract string GetFullName(bool reversed = false);
+
+    }
+}
