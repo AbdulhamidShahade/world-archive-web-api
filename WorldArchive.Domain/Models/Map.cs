3a7784a7e3a78bd92eb9c01d0f29f9ffa39f6e1f
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-08-29 15:50:34 +0300

    Add map domain model

---
+++ WorldArchive.Domain/Models/Map.cs
@@ -0,0 +1,10 @@
+﻿using WorldArchive.Domain.Models.Base;
+
+namespace WorldArchive.Domain.Models
+{
+    public class Map : EntityBase
+    {
+        public string? GoogleMap { get; set; }
+        public string? OpenStreetMap { get; set; }
+    }
+}
