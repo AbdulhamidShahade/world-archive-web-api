36aef6e4940ef16148f6353a427e926df639deae
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-08-29 15:49:38 +0300

    Add city domain model

---
+++ WorldArchive.Domain/Models/City.cs
@@ -0,0 +1,15 @@
+﻿using WorldArchive.Domain.Models.Base;
+
+namespace WorldArchive.Domain.Models
+{
+    public class City : EntityBase
+    {
+        public string Name { get; set; }
+        public int Population { get; set; }
+        public Map Map { get; set; }
+        public Country Country { get; set; }
+
+
+        public List<University> UniversitesByCity { get; set; }
+    }
+}
