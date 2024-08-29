7a6f4e0c4ee36cef69213e264363e4c722cb96f7
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-08-29 15:50:43 +0300

    Add movie domain model

---
+++ WorldArchive.Domain/Models/Movie.cs
@@ -0,0 +1,14 @@
+﻿using WorldArchive.Domain.Models.Base;
+
+namespace WorldArchive.Domain.Models
+{
+    public class Movie : EntityBase
+    {
+        public string Title { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string Description { get; set; }
+
+        public List<MovieLanguage> LanguagesByMovie { get; set; }
+
+    }
+}
