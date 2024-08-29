d168bfbaed48301a5d62e87de0df9e13c6199e42
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-08-29 15:52:18 +0300

    Add constants

---
+++ WorldArchive.Domain/Constants/Day.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorldArchive.Domain.Constants
+{
+    public enum Day
+    {
+        SUNDAY = 1,
+        MONDAY,
+        TUESDAY,
+        WEDNESDAY,
+        THURSDAY,
+        FRIDAY,
+        SATURDAY
+    };
+}
---
+++ WorldArchive.Domain/Constants/Month.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorldArchive.Domain.Constants
+{
+    public enum Month
+    {
+        JANUARY = 1,
+        FEBUARY,
+        MARCH, 
+        APRIL,
+        MAY,
+        JUNE,
+        JULY,
+        AUGUST,
+        SEPTEMBER,
+        NOVEMBER,
+        OCTOBER,
+        DECEMBER
+    }
+}
