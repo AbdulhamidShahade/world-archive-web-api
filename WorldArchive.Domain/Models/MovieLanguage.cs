8c5dcd4e682367e348e2e8a6a4955033d59e2367
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-08-29 15:51:53 +0300

    Add MovieLanguage model

---
+++ WorldArchive.Domain/Models/MovieLanguage.cs
@@ -0,0 +1,14 @@
+﻿using WorldArchive.Domain.Models.Base;
+
+namespace WorldArchive.Domain.Models
+{
+    public class MovieLanguage : EntityBase
+    {
+        public int MovieId { get; set; }
+        public Movie Movie { get; set; }
+
+
+        public int LanguageId { get; set; }
+        public Language Language { get; set; }
+    }
+}
