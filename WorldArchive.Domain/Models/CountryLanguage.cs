d9276d3d9467d2df6751d0d46acc2ebcadfcfb4d
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-08-29 15:51:44 +0300

    Add CountryLanguage model

---
+++ WorldArchive.Domain/Models/CountryLanguage.cs
@@ -0,0 +1,14 @@
+﻿using WorldArchive.Domain.Models.Base;
+
+namespace WorldArchive.Domain.Models
+{
+    public class CountryLanguage : EntityBase
+    {
+        public int CountryId { get; set; }
+        public Country Country { get; set; }
+
+
+        public int LanguageId { get; set; }
+        public Language Language { get; set; }
+    }
+}
