cb0a1d5433ccaef39702f65c7782ab27b6fddbe0
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-08-31 23:51:06 +0300

    Add Persistence registration with services

---
+++ WorldArchive.Persistence/PersistenceRegistration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorldArchive.Persistence.Data;
+
+namespace WorldArchive.Persistence
+{
+    public static class PersistenceRegistration
+    {
+        public static IServiceCollection AddPersistenceServices(this IServiceCollection serviceDescriptors, IConfiguration configuration)
+        {
+            serviceDescriptors.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("MSSQLServer"));
+            });
+
+            return serviceDescriptors;
+        }
+    }
+}
